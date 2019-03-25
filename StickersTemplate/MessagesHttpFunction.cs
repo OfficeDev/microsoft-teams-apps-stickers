d2ea71c0875bb10a9627e501b333606770463dcb
Matt Stankiewicz <mastank@microsoft.com>
2019-03-25 11:05:35 -0700

    Fix Auth Header bug, write tests and fix a few other bugs found during test

--- StickersTemplate/MessagesHttpFunction.cs
+++ StickersTemplate/MessagesHttpFunction.cs
@@ -7,6 +7,7 @@
 namespace StickersTemplate
 {
     using System;
+    using System.Diagnostics.CodeAnalysis;
     using System.IO;
     using System.Linq;
     using System.Threading.Tasks;
@@ -31,6 +32,36 @@ namespace StickersTemplate
     /// </summary>
     public static class MessagesHttpFunction
     {
+        private static ISettings settings;
+        private static IStickerSetRepository stickerSetRepository;
+        private static IStickerSetIndexer stickerSetIndexer;
+        private static ICredentialProvider credentialProvider;
+        private static IChannelProvider channelProvider;
+
+        /// <summary>
+        /// Configures the services used by this Azure function for a test run.
+        /// This is necessary because of the static nature of Azure functions (and lack of DI).
+        /// </summary>
+        /// <param name="settings">The <see cref="ISettings"/>.</param>
+        /// <param name="stickerSetRepository">The <see cref="IStickerSetRepository"/>.</param>
+        /// <param name="stickerSetIndexer">The <see cref="IStickerSetIndexer"/>.</param>
+        /// <param name="credentialProvider">The <see cref="ICredentialProvider"/>.</param>
+        /// <param name="channelProvider">The <see cref="IChannelProvider"/>.</param>
+        [ExcludeFromCodeCoverage]
+        public static void ConfigureForTests(
+            ISettings settings,
+            IStickerSetRepository stickerSetRepository,
+            IStickerSetIndexer stickerSetIndexer,
+            ICredentialProvider credentialProvider,
+            IChannelProvider channelProvider)
+        {
+            MessagesHttpFunction.settings = settings ?? throw new ArgumentNullException(nameof(settings));
+            MessagesHttpFunction.stickerSetRepository = stickerSetRepository ?? throw new ArgumentNullException(nameof(stickerSetRepository));
+            MessagesHttpFunction.stickerSetIndexer = stickerSetIndexer ?? throw new ArgumentNullException(nameof(stickerSetIndexer));
+            MessagesHttpFunction.credentialProvider = credentialProvider ?? throw new ArgumentNullException(nameof(credentialProvider));
+            MessagesHttpFunction.channelProvider = channelProvider ?? throw new ArgumentNullException(nameof(channelProvider));
+        }
+
         /// <summary>
         /// Method that is called by the Azure Function framework when this Azure Function is invoked.
         /// </summary>
@@ -44,15 +75,31 @@ namespace StickersTemplate
             ILogger logger,
             ExecutionContext context)
         {
+            if (logger == null)
+            {
+                throw new ArgumentNullException(nameof(logger));
+            }
+
             using (var scope = logger.BeginScope($"{nameof(MessagesHttpFunction.Run)}"))
             {
+                if (req == null)
+                {
+                    throw new ArgumentNullException(nameof(req));
+                }
+
+                if (context == null)
+                {
+                    throw new ArgumentNullException(nameof(context));
+                }
+
                 logger.LogInformation("Messages function received a request.");
 
-                ISettings settings = new Settings(logger, context);
-                IStickerSetRepository stickerSetRepository = new StickerSetRepository(logger, settings);
-                IStickerSetIndexer stickerSetIndexer = new StickerSetIndexer(logger);
-                ICredentialProvider credentialProvider = new SimpleCredentialProvider(settings.MicrosoftAppId, null);
-                IChannelProvider channelProvider = new SimpleChannelProvider();
+                // Use the configured service for tests or create the real one to use.
+                ISettings settings = MessagesHttpFunction.settings ?? new Settings(logger, context);
+                IStickerSetRepository stickerSetRepository = MessagesHttpFunction.stickerSetRepository ?? new StickerSetRepository(logger, settings);
+                IStickerSetIndexer stickerSetIndexer = MessagesHttpFunction.stickerSetIndexer ?? new StickerSetIndexer(logger);
+                ICredentialProvider credentialProvider = MessagesHttpFunction.credentialProvider ?? new SimpleCredentialProvider(settings.MicrosoftAppId, null);
+                IChannelProvider channelProvider = MessagesHttpFunction.channelProvider ?? new SimpleChannelProvider();
 
                 Activity activity;
                 try
@@ -78,8 +125,12 @@ namespace StickersTemplate
                     return new BadRequestObjectResult($"App only supports compose extension query activity types.");
                 }
 
-                var queryValue = JObject.FromObject(activity.Value).ToObject<ComposeExtensionValue>();
-                var query = queryValue.GetParameterValue();
+                var query = string.Empty;
+                if (activity.Value != null)
+                {
+                    var queryValue = JObject.FromObject(activity.Value).ToObject<ComposeExtensionValue>();
+                    query = queryValue.GetParameterValue();
+                }
 
                 var stickerSet = await stickerSetRepository.FetchStickerSetAsync();
                 await stickerSetIndexer.IndexStickerSetAsync(stickerSet);
@@ -107,7 +158,7 @@ namespace StickersTemplate
         /// <returns>The authorization header.</returns>
         private static string GetAuthorizationHeader(HttpRequest req)
         {
-            if (!req.Headers.TryGetValue("Authorization", out var authHeaders) && authHeaders.Count < 1)
+            if (req.Headers == null || !req.Headers.TryGetValue("Authorization", out var authHeaders) || authHeaders.Count < 1)
             {
                 throw new UnauthorizedAccessException();
             }
@@ -123,6 +174,11 @@ namespace StickersTemplate
         /// <returns>The <see cref="Activity"/> object.</returns>
         private static async Task<Activity> ParseRequestBody(HttpRequest req)
         {
+            if (req.Body == null)
+            {
+                throw new InvalidOperationException($"{nameof(req)}.{nameof(req.Body)} cannot be null");
+            }
+
             using (var streamReader = new StreamReader(req.Body))
             using (var jsonReader = new JsonTextReader(streamReader))
             {
