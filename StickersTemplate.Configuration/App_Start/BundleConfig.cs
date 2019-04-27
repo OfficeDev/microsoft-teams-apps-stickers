4a77b6022947b41d1b3aede6313e1a631dc4f44a
Adrian Solis <aosolis@microsoft.com>
2019-04-26 19:36:55 -0700

    Remove client-side libraries from the repo (#10)
    Use libman to fetch client-side libraries
    Add build step to restore client-side libraries

--- StickersTemplate.Configuration/App_Start/BundleConfig.cs
+++ StickersTemplate.Configuration/App_Start/BundleConfig.cs
@@ -21,21 +21,22 @@ namespace StickersTemplate.Configuration
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/lib/jquery/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/lib/jquery-validate/jquery.validate.js",
+                        "~/lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/lib/modernizr/modernizr.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/lib/bootstrap/dist/js/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/lib/bootstrap/dist/css/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
